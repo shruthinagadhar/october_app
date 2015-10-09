a88bbd173569f8debb5dde1c400307b05c9e2c00
shruthinagadhar <shruthi1234@yopmail.com>
2015-10-09 20:49:42 +0530

    second commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OctoberShruthi::Application.config.secret_token = '5fe9535068aaa63da222123ac9bf805eb0c50c90304d13b53e847b5608fdb873721c5a7e76c3e847255b912e7de6ae5086bb55413307d43a1a261158326bba86'
